a4252f1c722a784d82d3c7a927fbc0bf63338326
Joe Combopiano <jcombopi@impinjadmin1s-MacBook-Pro-8.local>
2016-03-08 09:39:54 -0800

    updates to use server.js instead of bin/www

--- readme.md
+++
@@ -1,2 +0,0 @@
-The healthcare REC demo software
-
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('app');
+var app = require('./app');
 var debug = require('debug')('zoner:server');
 var http = require('http');
 
