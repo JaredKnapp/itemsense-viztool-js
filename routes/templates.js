c8aecbcec21548aa84199c5c06d066605d879e02
Reza Alemy <reza@alemy.net>
2017-01-14 09:52:20 -0800

    template server refactored to accept pug extension as well as jade

--- routes/templates.js
+++ routes/templates.js
@@ -7,17 +7,28 @@ var express = require("express"),
     fs = require("fs"),
     path = require("path"),
     jade = require("jade"),
+    q = require("q"),
     router = express.Router();
 
+function promiseReadFile(filename, encoding){
+    let defer = q.defer();
+    if(!encoding) encoding = "utf8";
+    fs.readFile(filename,encoding,defer.makeNodeResolver());
+    return defer.promise;
+}
+
 function renderJade(req, res) {
-    var templateFileName = path.resolve(path.dirname(__dirname),
-        "views", "templates", req.params[0] + ".jade");
-    fs.readFile(templateFileName, "utf8", function (err, data) {
-        if (err)
-            return res.status(404).send("<pre> File Not Found: " + req.params[0] + ".jade\n" + err + "</pre>");
-        var templateFunction = jade.compile(data, {filename: templateFileName});
-        res.status(200).send(templateFunction(req.body));
-    });
+    let templateFileName = path.resolve(path.dirname(__dirname),
+        "views", "templates", req.params[0] );
+    return promiseReadFile(templateFileName + ".jade")
+        .catch(error => promiseReadFile(templateFileName + ".pug"))
+        .then(data => {
+            res.status(200).send(jade.compile(data,{filename: templateFileName})(req.body));
+        })
+        .catch(error => {
+            console.log("Jade error", error);
+            res.status(500).send(error);
+        });
 }
 router.get("/*", function (req, res) {
     renderJade(req, res);
