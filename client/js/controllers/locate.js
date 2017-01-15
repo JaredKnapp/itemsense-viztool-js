607783accea23031e7b3a3a6c9b3b0664cf7652d
Reza Alemy <reza@alemy.net>
2017-01-14 17:30:05 -0800

    refactored to separete file for locate controller and factory

---
+++ client/js/controllers/locate.js
@@ -0,0 +1,43 @@
+/**
+ * Created by ralemy on 1/14/17.
+ */
+"use strict";
+
+module.exports = (function (app) {
+    app.controller("Locate", ["$scope", "LocateFactory",function ($scope, factory) {
+
+        $scope.getLocateAreas = function () {
+            $scope.project.callRest({url: "/locate/areas"}).then(
+                success => $scope.tree = factory.constructTree(success.Items),
+                failure => console.log("failed getting from locate", failure)
+            );
+        };
+
+        $scope.selectNode = function (v) {
+            $scope.selectedNode = v;
+        };
+
+        $scope.reportImages = function (v) {
+            if (v.children)
+                if (!v.images.length)
+                    return "(<small>No images</small>)";
+                else if (v.images.length === 1)
+                    return `<small>${v.images[0]}</small>`;
+                else
+                    return `<small>${v.images.length} images</small>`;
+            return "";
+        };
+        $scope.notSuitable=function(){
+            if(!$scope.selectedNode)
+                return "No Area Selected";
+            if($scope.selectedNode.Level !== "Floor")
+                return "Not a Floor Level Node";
+            if($scope.selectedNode.images.length !== 1)
+                return "Must have one image";
+            return false;
+        };
+        $scope.importFromLocate = function(){
+            factory.importFromLocate($scope.project,$scope.selectedNode);
+        };
+    }]);
+})(angular.module(window.mainApp));
---
+++ client/js/services/locate.js
@@ -0,0 +1,41 @@
+/**
+ * Created by ralemy on 1/14/17.
+ */
+"use strict";
+
+module.exports = (function (app) {
+    app.factory("LocateFactory", ["_", function (_) {
+
+        function collectImages(root) {
+            let images = [];
+            _.each(root.children, v => images = images.concat(collectImages(v)));
+            root.images = _.uniq(root.ImageId ? images.concat(root.ImageId) : images);
+            return root.images;
+        }
+
+        return {
+            constructTree(areaArray) {
+                let hashMap = _.reduce(areaArray, (result, area) => {
+                    result[area.AreaId] = area;
+                    return result;
+                }, {});
+                let tree = _.reduce(areaArray, (tree, area) => {
+                    let parentId = area.ParentId;
+                    if (!parentId) tree[area.AreaId] = area;
+                    else {
+                        let parent = hashMap[parentId];
+                        if (!parent.children)
+                            parent.children = {};
+                        parent.children[area.AreaId] = area;
+                    }
+                    return tree;
+                }, {});
+                _.each(tree, v => collectImages(v));
+                return tree;
+            },
+            importFromLocate(project, node){
+                console.log("importing from locate node ", node);
+            }
+        }
+    }]);
+})(angular.module(window.mainApp));
